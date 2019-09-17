7c492e07252c8414db22265d02ee2895731fd200
Danielle Subang <daniellesubang@gmail.com>
2019-09-17 12:25:16 -0700

    Delete FileService.js

--- app/src/Components/FileService.js
+++
@@ -1,8 +0,0 @@
-function FileService(params) {
-	var xhr = new XMLHttpRequest();
-	xhr.open(params.method, params.url);
-	xhr.overrideMimeType(params.data.type);
-	xhr.send(params.data);
-}
-
-module.exports = FileService;
